44740a98f2afd4f4bd7afeafaedd2a4c4ce7041e
beppo-sturmtief <beppo.sturmtief@gmail.com>
2023-12-23 14:27:16 +0100

    next try,still exploding

--- 21/solutions.js
+++ 21/solutions.js
@@ -5,11 +5,22 @@ const nonRocks = grid.flatMap(r=>r.filter(c=>c.noRock));
 
 const start = nonRocks.find(nr=>nr.start);
 
-nonRocks.forEach(nr=>nr[1]=[[0,-1],[1,0],[0,1],[-1,0]].map(([x,y])=>[x+nr.x,y+nr.y]).filter(([x,y])=>grid[y]&&grid[y][x]&&grid[y][x].noRock).map(([x,y])=>grid[y][x]));
+nonRocks.forEach(nr=>nr[1]=new Set([[0,-1],[1,0],[0,1],[-1,0]].map(([x,y])=>[x+nr.x,y+nr.y]).filter(([x,y])=>grid[y]&&grid[y][x]&&grid[y][x].noRock).map(([x,y])=>grid[y][x])));
 
-const step = (nr,n)=>nr[n]=[...new Set(nr[n>>1].flatMap(nnr=>nnr[n>>1]))];
+const step = (nr,n)=> {
+    nr[n] = new Set();
+    nr[n>>1]
+        .forEach(nnr=>nnr[n>>1]
+             .forEach(
+                 nnnr=>nr[n].add(nnnr)
+             )
+        );};
+     
 
-for (let n = 2; n <= 64; n <<= 1)
+for (let n = 2; n <= 32; n <<= 1)
     nonRocks.forEach(nr=>step(nr,n));
 
-const solutionPart1 = start[64].length;
+step(start, 64);
+
+const solutionPart1 = start[64].size();
+console.log(solutionPart1);
